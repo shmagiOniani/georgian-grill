1b2e761b32d9002f036418e86de28d7a40d7e5a5
Shmagi Oniani <onianishmagi@gmail.com>
2022-01-29 01:13:49 +0400

    add font transformation

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { routes } from "./routes/routes";
 import "./App.scss";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import Test from "./pages/Test";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route exact path="/">
             <Redirect to="/" />  <Home />
           </Route>
+          <Route exact path="/test" component={Test}/>
           {routes.map((item) => (
             <Route
               exact={item.exact}
---
+++ src/pages/Test.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Test.scss"
+
+function Test() {
+  return (
+    <div className="test-container">
+      <div className="container">
+        <h1>header 1</h1>
+        <h2>header 2</h2>
+        <h3>header 3</h3>
+        <h4>header 4</h4>
+        <h5>header 5</h5>
+        <p>paragraph</p>
+      </div>
+      <div className="container">
+        <h1>სათაური 1</h1>
+        <h2>სათაური 2</h2>
+        <h3>სათაური 3</h3>
+        <h4>სათაური 4</h4>
+        <h5>სათაური 5</h5>
+        <p>პარაგრაფი</p>
+      </div>
+    </div>
+  );
+}
+
+export default Test;
